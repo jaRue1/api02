eba979a159bb9abfd7643c9f17d8949a2fb0f496
jaRue1 <jaruejohnson@outlook.com>
2021-04-20 11:08:07 -0400

    added express

--- functions/index.js
+++ functions/index.js
@@ -1,5 +1,11 @@
 const functions = require("firebase-functions")
-
-exports.helloWorld = functions.https.onRequest((request, response) => {
-  response.send("Hello from Firebase!")
+const express = require('express')
+const app = express()
+app.get('/test', (req,res) => {
+  res.send('it is working !')
 })
+app.get('/test2', (req,res) => {
+  res.send('this is test 2')
+})
+exports.app = functions.https.onRequest(app)
+
